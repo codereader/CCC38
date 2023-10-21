5aeba76c2921b7ad219d222268804cf953dee4ce
codereader <greebo@angua.at>
2023-10-21 07:09:25 +0200

    Add previous levels back in

--- CCC/Program.cs
+++ CCC/Program.cs
@@ -7,12 +7,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //Level1();
-        //Level2();
-        //Level3();
-        //Level4();
-        //Level5_6("5");
-        //Level5_6("6");
+        Level1();
+        Level2();
+        Level3();
+        Level4();
+        Level5_6("5");
+        Level5_6("6");
         Level7();
 
         Console.WriteLine("Done");
@@ -636,5 +636,481 @@ public class Program
         }
     }
 
+    private static void Level5_6(string level)
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level{level}_{inputFileNumber}.in";
+            var outputfilename = $"../../../level{level}_{inputFileNumber}.out";
 
+            Console.WriteLine(inputfilename);
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            int mapHeight = int.Parse(lines.First());
+            var map = lines.Skip(1).Take(mapHeight).ToList();
+            var mapWidth = map.First().Length;
+
+            var inputs = lines.Skip(1 + mapHeight + 1).ToList();
+            var output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                var coords1 = input.Split(',').Select(int.Parse).ToArray();
+                var startPos = new Vector2(coords1[0], coords1[1]);
+
+                var availablePositions = new Stack<Vector2>();
+                availablePositions.Push(startPos);
+
+                var islandPositions = new HashSet<Vector2>();
+
+                var floodFillDirections = new List<Vector2>()
+                {
+                    {new Vector2(1, 0) },
+                    {new Vector2(-1, 0) },
+                    {new Vector2(0, 1) },
+                    {new Vector2(0, -1) }
+                };
+
+                // flood fill island
+                while (availablePositions.Count > 0)
+                {
+                    var nextPos = availablePositions.Pop();
+
+                    islandPositions.Add(nextPos); // mark as done
+
+                    foreach (var dir in floodFillDirections)
+                    {
+                        var candidate = nextPos + dir;
+
+                        if (candidate.Y < 0 || candidate.Y >= map.Count ||
+                            candidate.X < 0 || candidate.X >= mapWidth)
+                        {
+                            continue; // out of bounds
+                        }
+
+                        if (!islandPositions.Contains(candidate) && map[(int)candidate.Y][(int)candidate.X] == 'L')
+                        {
+                            availablePositions.Push(candidate);
+                        }
+                    }
+                }
+
+                var minX = islandPositions.Min(p => p.X);
+                var maxX = islandPositions.Max(p => p.X);
+                var minY = islandPositions.Min(p => p.Y);
+                var maxY = islandPositions.Max(p => p.Y);
+
+                var validBounds = new Bounds(minX: minX - 1, minY: minY - 1, maxX: maxX + 1, maxY: maxY + 1);
+
+                var leftEdge = islandPositions.First(p => p.X == minX);
+                var firstWaterTile = new Vector2(leftEdge.X - 1, leftEdge.Y);
+
+                if (leftEdge.X < 0) throw new ArgumentOutOfRangeException("Start X out of range");
+
+                // find sea route around the island
+                var startRoute = new Route();
+                startRoute.AddStartPoint(firstWaterTile);
+
+                var routesToInvestigate = new SortedList<int, List<Route>>();
+                routesToInvestigate.Add(startRoute.Length, new List<Route> { startRoute });
+
+                var visitedPoints = new HashSet<Vector2>();
+                visitedPoints.Add(firstWaterTile);
+
+                Route? winningRoute = null;
+
+                var StartingDirections = new List<Vector2>
+                {
+                    new Vector2(0, +1),
+                    new Vector2(+1, +1),
+                };
+
+                // First round we only go to the south or south east
+                var directions = StartingDirections;
+
+                while (routesToInvestigate.Count > 0)
+                {
+                    // Pick one of the shortest routes first
+                    var shortestRoutes = routesToInvestigate.First().Value;
+
+                    var route = shortestRoutes[shortestRoutes.Count - 1];
+                    shortestRoutes.RemoveAt(shortestRoutes.Count - 1);
+
+                    if (shortestRoutes.Count == 0)
+                    {
+                        routesToInvestigate.Remove(route.Length);
+                    }
+
+                    var currentPos = route.EndPoint;
+
+                    foreach (var direction in directions)
+                    {
+                        var targetPoint = currentPos + direction;
+
+                        if (targetPoint.Y < validBounds.Min.Y || targetPoint.Y > validBounds.Max.Y ||
+                            targetPoint.X < validBounds.Min.X || targetPoint.X > validBounds.Max.X)
+                        {
+                            continue; // out of valid bounds
+                        }
+
+                        if (visitedPoints.Contains(targetPoint))
+                        {
+                            if (targetPoint == firstWaterTile && RouteIsValid(route, validBounds))
+                            {
+                                // Don't add the starting point to the route
+                                winningRoute = route;
+                                routesToInvestigate.Clear();
+                                break;
+                            }
+
+                            continue;
+                        }
+
+                        if (map[(int)targetPoint.Y][(int)targetPoint.X] == 'L')
+                        {
+                            visitedPoints.Add(targetPoint);
+                            continue;
+                        }
+
+                        // If the route is crossing itself, discard
+                        if (route.PointIsCrossing(currentPos, targetPoint))
+                        {
+                            visitedPoints.Add(targetPoint);
+                            continue; // route would cross itself
+                        }
+
+                        var newRoute = route.Clone();
+
+                        newRoute.AddPoint(targetPoint);
+
+                        if (!routesToInvestigate.TryGetValue(newRoute.Length, out var targetRoutes))
+                        {
+                            targetRoutes = new List<Route>();
+                            routesToInvestigate.Add(newRoute.Length, targetRoutes);
+                        }
+                        targetRoutes.Add(newRoute);
+                        visitedPoints.Add(targetPoint);
+                    }
+
+                    directions = WaterDirections; // From now on consider all options
+                }
+
+                if (winningRoute == null)
+                {
+                    throw new InvalidOperationException("No solution found");
+                }
+
+                output.AppendLine(winningRoute.ToString());
+            }
+
+            outputWriter.Write(output.ToString());
+        }
+    }
+
+    private static void Level4()
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level4_{inputFileNumber}.in";
+            var outputfilename = $"../../../level4_{inputFileNumber}.out";
+
+            Console.WriteLine(inputfilename);
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            int mapHeight = int.Parse(lines.First());
+            var map = lines.Skip(1).Take(mapHeight).ToList();
+            var mapWidth = map.First().Length;
+
+            var inputs = lines.Skip(1 + mapHeight + 1).ToList();
+            var output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                var coordPair = input.Split(' ').ToArray();
+                var coords1 = coordPair[0].Split(',').Select(int.Parse).ToArray();
+                var startPos = new Vector2(coords1[0], coords1[1]);
+
+                var coords2 = coordPair[1].Split(',').Select(int.Parse).ToArray();
+                var endPos = new Vector2(coords2[0], coords2[1]);
+
+                var startRoute = new Route();
+                startRoute.AddStartPoint(startPos);
+
+                var visitedPoints = new HashSet<Vector2>();
+
+                var routesToInvestigate = new SortedList<int, List<Route>>();
+
+                routesToInvestigate.Add(startRoute.Length, new List<Route> { startRoute });
+
+                var directions = WaterDirections;
+
+                Route? winningRoute = null;
+
+                while (routesToInvestigate.Count > 0)
+                {
+                    // Pick one of the shortest routes first
+                    var shortestRoutes = routesToInvestigate.First().Value;
+
+                    var route = shortestRoutes[shortestRoutes.Count - 1];
+                    shortestRoutes.RemoveAt(shortestRoutes.Count - 1);
+
+                    if (shortestRoutes.Count == 0)
+                    {
+                        routesToInvestigate.Remove(route.Length);
+                    }
+
+                    var currentPos = route.EndPoint;
+
+                    foreach (var direction in directions)
+                    {
+                        var targetPoint = currentPos + direction;
+
+                        if (targetPoint.Y < 0 || targetPoint.Y >= map.Count ||
+                            targetPoint.X < 0 || targetPoint.X >= mapWidth)
+                        {
+                            continue; // out of bounds
+                        }
+
+                        if (visitedPoints.Contains(targetPoint))
+                        {
+                            continue;
+                        }
+
+                        if (targetPoint == endPos)
+                        {
+                            route.AddPoint(targetPoint);
+                            winningRoute = route;
+                            routesToInvestigate.Clear();
+                            break;
+                        }
+
+                        if (map[(int)targetPoint.Y][(int)targetPoint.X] == 'L')
+                        {
+                            visitedPoints.Add(targetPoint);
+                            continue;
+                        }
+
+                        // If the route is crossing itself, discard
+                        if (route.PointIsCrossing(currentPos, targetPoint))
+                        {
+                            visitedPoints.Add(targetPoint);
+                            continue; // route would cross itself
+                        }
+
+                        var newRoute = route.Clone();
+
+                        newRoute.AddPoint(targetPoint);
+
+                        if (!routesToInvestigate.TryGetValue(newRoute.Length, out var targetRoutes))
+                        {
+                            targetRoutes = new List<Route>();
+                            routesToInvestigate.Add(newRoute.Length, targetRoutes);
+                        }
+                        targetRoutes.Add(newRoute);
+                        visitedPoints.Add(targetPoint);
+                    }
+                }
+
+                if (winningRoute == null)
+                {
+                    throw new InvalidOperationException("No solution found");
+                }
+
+                output.AppendLine(winningRoute.ToString());
+            }
+
+            outputWriter.Write(output.ToString());
+        }
+    }
+
+    private static void Level3()
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level3_{inputFileNumber}.in";
+            var outputfilename = $"../../../level3_{inputFileNumber}.out";
+
+            Console.WriteLine(inputfilename);
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            int mapHeight = int.Parse(lines.First());
+
+            var map = lines.Skip(1).Take(mapHeight).ToList();
+
+            var mapWidth = map.First().Length;
+
+            var inputs = lines.Skip(1 + mapHeight + 1).ToList();
+            var output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                var coordPairs = input.Split(' ').ToArray();
+
+                var visitedPositions = new HashSet<Vector2>();
+                var intersectsWithItself = false;
+
+                var startCoords = coordPairs.First().Split(',').Select(int.Parse).ToArray();
+                var startPos = new Vector2(startCoords[0], startCoords[1]);
+                visitedPositions.Add(startPos);
+
+                var lastPos = startPos;
+
+                foreach (var coordPair in coordPairs.Skip(1))
+                {
+                    var coords = coordPair.Split(',').Select(int.Parse).ToArray();
+                    var pos = new Vector2(coords[0], coords[1]);
+
+                    if (visitedPositions.Contains(pos))
+                    {
+                        // Direct hit
+                        intersectsWithItself = true;
+                        break;
+                    }
+
+                    var halfPos = (pos + lastPos) / 2;
+
+                    if (visitedPositions.Contains(halfPos))
+                    {
+                        // Halfpos hit
+                        intersectsWithItself = true;
+                        break;
+                    }
+
+                    visitedPositions.Add(halfPos);
+                    visitedPositions.Add(pos);
+
+                    lastPos = pos;
+                }
+
+                output.AppendLine(intersectsWithItself ? "INVALID" : "VALID");
+            }
+
+            outputWriter.Write(output.ToString());
+        }
+    }
+
+    private static void Level2()
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level2_{inputFileNumber}.in";
+            var outputfilename = $"../../../level2_{inputFileNumber}.out";
+
+            Console.WriteLine(inputfilename);
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            int mapHeight = int.Parse(lines.First());
+
+            var map = lines.Skip(1).Take(mapHeight).ToList();
+
+            var inputs = lines.Skip(1 + mapHeight + 1).ToList();
+            var output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                var coordPair = input.Split(' ').ToArray();
+                var coords1 = coordPair[0].Split(',').Select(int.Parse).ToArray();
+                var pos1 = new Vector2(coords1[0], coords1[1]);
+
+                var coords2 = coordPair[1].Split(',').Select(int.Parse).ToArray();
+                var pos2 = new Vector2(coords2[0], coords2[1]);
+
+                var availablePositions = new Stack<Vector2>();
+                availablePositions.Push(pos1);
+
+                var visitedPositions = new HashSet<Vector2>();
+
+                bool isOnSameIsland = false;
+
+                var directions = new List<Vector2>()
+                {
+                    {new Vector2(1, 0) },
+                    {new Vector2(-1, 0) },
+                    {new Vector2(0, 1) },
+                    {new Vector2(0, -1) }
+                };
+
+                var mapWidth = map.First().Length;
+
+                while (availablePositions.Count > 0)
+                {
+                    var nextPos = availablePositions.Pop();
+
+                    visitedPositions.Add(nextPos); // mark as done
+
+                    if (nextPos == pos2)
+                    {
+                        isOnSameIsland = true;
+                        break;
+                    }
+
+                    foreach (var dir in directions)
+                    {
+                        var candidate = nextPos + dir;
+
+                        if (candidate.Y < 0 || candidate.Y >= map.Count ||
+                            candidate.X < 0 || candidate.X >= mapWidth)
+                        {
+                            continue; // out of bounds
+                        }
+
+                        if (!visitedPositions.Contains(candidate) && map[(int)candidate.Y][(int)candidate.X] == 'L')
+                        {
+                            availablePositions.Push(candidate);
+                        }
+                    }
+                }
+
+                output.AppendLine(isOnSameIsland ? "SAME" : "DIFFERENT");
+            }
+
+            outputWriter.Write(output.ToString());
+        }
+    }
+
+    private static void Level1()
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level1_{inputFileNumber}.in";
+            var outputfilename = $"../../../level1_{inputFileNumber}.out";
+
+            Console.WriteLine(inputfilename);
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            int mapHeight = int.Parse(lines.First());
+
+            var map = lines.Skip(1).Take(mapHeight).ToList();
+
+            var inputs = lines.Skip(1 + mapHeight + 1).ToList();
+            var output = new StringBuilder();
+
+            foreach (var input in inputs)
+            {
+                var coords = input.Split(',').Select(int.Parse).ToArray();
+                var x = coords[0];
+                var y = coords[1];
+
+                output.Append(map[y][x]);
+                output.AppendLine();
+            }
+
+            outputWriter.Write(output.ToString());
+        }
+    }
 }
