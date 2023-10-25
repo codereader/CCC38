c028dde7da4daa81dde0da4ef42afcecec9cfe78
Angua <birgit@angua.at>
2023-10-25 13:55:55 +0200

    Add new WPF project

---
+++ CCCIslands/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CCCIslands;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainViewModel = new MainViewModel();
+        var window = new MainWindow()
+        {
+            DataContext = mainViewModel
+        };
+        window.Show();
+    }
+}
